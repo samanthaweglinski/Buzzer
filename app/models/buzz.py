eb887e8c7ec947966dec35b437bd7de66215593c
Samantha Weglinski <samantha.weglinski@gmail.com>
2022-08-31 12:57:20 -0700

    Fixed synntax ug in comment.py

--- app/models/buzz.py
+++ app/models/buzz.py
@@ -7,7 +7,7 @@ class Buzz(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(255), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-    image_url = db.Columb(db.String(255))
+    image_url = db.Column(db.String(255))
 
     user = db.relationship('User', back_populates='buzzes')
 
--- app/models/comment.py
+++ app/models/comment.py
@@ -7,7 +7,7 @@ class Comment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(255), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-    buzz_id = db.Columb(db.Integer, db.ForeignKey('buzzes.id'), nullable=False)
+    buzz_id = db.Column(db.Integer, db.ForeignKey('buzzes.id'), nullable=False)
 
     user = db.relationship('User', back_populates='comments')
     buzz = db.relationship('Buzz', back_populates='comments')
