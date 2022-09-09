b004906f024d271897c74503f5f1b2bf0e4d7c1d
Samantha Weglinski <samantha.weglinski@gmail.com>
2022-09-08 23:42:23 -0700

    removed email-validator from signup_form to fix heroku bug

--- app/forms/signup_form.py
+++ app/forms/signup_form.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField
-from wtforms.validators import DataRequired, ValidationError, Length, Regexp, EqualTo, Email
+from wtforms.validators import DataRequired, ValidationError, Length, Regexp, EqualTo
+# from wtforms.validators import DataRequired, ValidationError, Length, Regexp, EqualTo, Email
 from app.models import User
 
 
@@ -23,7 +24,8 @@ def username_exists(form, field):
 class SignUpForm(FlaskForm):
     username = StringField(
         'username', validators=[DataRequired("Please enter a username"), Length(min=3, max=40, message="Username must be 3-40 characters long"), username_exists])
-    email = StringField('email', validators=[DataRequired("Please enter your email address"), user_exists, Email("Please provide a valid email"), Length(max=40, message="Email must be less than 40 characters")])
+    # email = StringField('email', validators=[DataRequired("Please enter your email address"), user_exists, Email("Please provide a valid email"), Length(max=40, message="Email must be less than 40 characters")])
+    email = StringField('email', validators=[DataRequired("Please enter your email address"), user_exists, Length(max=40, message="Email must be less than 40 characters")])
     profile_pic = StringField('profile_pic')
     bio = StringField('bio')
     password = StringField('password', validators=[DataRequired("Please enter a password"), Length(min=8, message='Please provide a password of at least 8 characters')])
