140a6dd3a01605e7776903bb66582fdab7394f54
Samantha Weglinski <samantha.weglinski@gmail.com>
2022-09-12 00:13:07 -0700

    Added 404 page

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,21 +1,21 @@
-import React, { useState, useEffect } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
-import NavBar from './components/NavBar';
-import ProtectedRoute from './components/auth/ProtectedRoute';
-import UsersList from './components/UsersList';
-import User from './components/User';
-import BuzzDetails from './components/BuzzDetails';
-import { authenticate } from './store/session';
-import SplashPage from './components/SplashPage';
-import HomePage from './components/HomePage/HomePage';
+import React, { useState, useEffect } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import NavBar from "./components/NavBar";
+import ProtectedRoute from "./components/auth/ProtectedRoute";
+import UsersList from "./components/UsersList";
+import User from "./components/User";
+import BuzzDetails from "./components/BuzzDetails";
+import { authenticate } from "./store/session";
+import SplashPage from "./components/SplashPage";
+import HomePage from "./components/HomePage/HomePage";
 
 function App() {
   const [loaded, setLoaded] = useState(false);
   const dispatch = useDispatch();
 
   useEffect(() => {
-    (async() => {
+    (async () => {
       await dispatch(authenticate());
       setLoaded(true);
     })();
@@ -28,20 +28,23 @@ function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path='/login' exact={true}>
-          <SplashPage/>
+        <Route path="/login" exact={true}>
+          <SplashPage />
         </Route>
-        <ProtectedRoute path='/users' exact={true} >
-          <UsersList/>
+        <ProtectedRoute path="/users" exact={true}>
+          <UsersList />
         </ProtectedRoute>
-        <ProtectedRoute path='/users/:userId' exact={true} >
+        <ProtectedRoute path="/users/:userId" exact={true}>
           <User />
         </ProtectedRoute>
-        <ProtectedRoute path='/' exact={true} >
+        <ProtectedRoute path="/" exact={true}>
           <HomePage />
         </ProtectedRoute>
-        <ProtectedRoute path='/buzzes/:buzzId' exact={true}>
+        <ProtectedRoute path="/buzzes/:buzzId" exact={true}>
           <BuzzDetails />
+        </ProtectedRoute>
+        <ProtectedRoute path="*">
+          <h1 className="page-not-found">404 Page Not Found</h1>
         </ProtectedRoute>
       </Switch>
     </BrowserRouter>
