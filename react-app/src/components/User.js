c7d57754588bf0b4db96629f8c63e99ec830fd84
Samantha Weglinski <samantha.weglinski@gmail.com>
2022-09-29 00:16:13 -0700

    Added containers to user profile page

--- react-app/src/components/User.js
+++ react-app/src/components/User.js
@@ -1,14 +1,17 @@
-import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import "./CSS/UserProfile.css";
+import NavBar from "./NavBar";
 
 function User() {
   const [user, setUser] = useState({});
-  const { userId }  = useParams();
+  const { userId } = useParams();
 
   useEffect(() => {
     if (!userId) {
       return;
     }
+
     (async () => {
       const response = await fetch(`/api/users/${userId}`);
       const user = await response.json();
@@ -21,17 +24,17 @@ function User() {
   }
 
   return (
-    <ul>
-      <li>
-        <strong>User Id:</strong> {userId}
-      </li>
-      <li>
-        <strong>Username:</strong> {user.username}
-      </li>
-      <li>
-        <strong>Email:</strong> {user.email}
-      </li>
-    </ul>
+    <div className="user-profile-container">
+      <div className="user-profile-left-container">
+        <NavBar />
+      </div>
+      <div className="user-profile-mid-container">
+
+      </div>
+      <div className="user-profile-right-container">
+
+      </div>
+    </div>
   );
 }
 export default User;
