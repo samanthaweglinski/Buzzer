31c45ceefb17e6b502fc474de2dc0755d23e3741
Samantha Weglinski <samantha.weglinski@gmail.com>
2022-09-30 20:41:27 -0700

    Continuing styling of create buzz form on homepage

--- react-app/src/components/HomePage/HomePage.js
+++ react-app/src/components/HomePage/HomePage.js
@@ -1,9 +1,12 @@
 import NavBar from "../NavBar";
 import BuzzForm from "./BuzzForm";
 import Buzzes from "./Buzzes";
+import { useSelector } from "react-redux";
 import "../CSS/HomePage.css";
 
 const HomePage = () => {
+  const user = useSelector((state) => state?.session?.user);
+
   return (
     <div className="homepage-container">
       <div className="homepage-left-container">
@@ -12,7 +15,16 @@ const HomePage = () => {
       <div className="homepage-mid-container">
         <div className="homepage-mid-top">
           <div className="home-text">Home</div>
-          <BuzzForm />
+          <div className="create-form-container">
+            <div className="current-user-pfp">
+              <img
+                src={user?.profile_pic}
+                alt="user-pfp"
+                className="create-buzz-user-pfp"
+              />
+            </div>
+            <BuzzForm />
+          </div>
         </div>
         <div className="homepage-mid-bottom">
           <Buzzes />
