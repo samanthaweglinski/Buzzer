bf581d5f8d8e655fddae77b27e9591146713f22e
Samantha Weglinski <samantha.weglinski@gmail.com>
2022-09-30 19:27:56 -0700

    Bottom left user info will show user profile when clicked

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import LogoutButton from "./auth/LogoutButton";
 import bird from "../components/images/twitter_logo.svg";
 import "./CSS/NavBar.css";
@@ -12,31 +12,44 @@ const NavBar = () => {
     <nav className="left-nav">
       <div className="nav-icons">
         <div className="bee-img">
-          <NavLink to="/" exact={true} activeClassName="active" className='home-link'>
+          <NavLink
+            to="/"
+            exact={true}
+            activeClassName="active"
+            className="home-link"
+          >
             <img className="buzzer-icon" src={bird} alt="" />
             <div className="link-description">Home</div>
           </NavLink>
         </div>
         <div className="linkedin">
-          <a href="https://www.linkedin.com/in/samanthaweglinski/" className="linkedin-link">
+          <a
+            href="https://www.linkedin.com/in/samanthaweglinski/"
+            className="linkedin-link"
+          >
             <i className="fa-brands fa-linkedin-in"></i>
             <div className="link-description">Linkedin</div>
           </a>
         </div>
         <div className="github">
-          <a href="https://github.com/samanthaweglinski" className="github-link">
+          <a
+            href="https://github.com/samanthaweglinski"
+            className="github-link"
+          >
             <i className="fa-brands fa-github"></i>
             <div className="link-description">Github</div>
           </a>
         </div>
         <div className="logout-button-component">
           <LogoutButton />
-          <div className="link-description">Logout</div>
+          {/* <div className="link-description">Logout</div> */}
         </div>
       </div>
       <div className="current-user-container">
-        <img src={user?.profile_pic} alt="user-pfp" className="user-pfp" />
-        <div className="current-user-username">@{user?.username}</div>
+        <Link to={`/users/${user?.id}`} className='current-user-container'>
+          <img src={user?.profile_pic} alt="user-pfp" className="user-pfp" />
+          <div className="current-user-username">@{user?.username}</div>
+        </Link>
       </div>
     </nav>
   );
