5b896983155da076822039f13b5e394336a92249
Samantha Weglinski <samantha.weglinski@gmail.com>
2022-09-30 20:33:41 -0700

    Working on CSS for create buzz form

--- react-app/src/components/HomePage/HomePage.js
+++ react-app/src/components/HomePage/HomePage.js
@@ -4,19 +4,21 @@ import Buzzes from "./Buzzes";
 import "../CSS/HomePage.css";
 
 const HomePage = () => {
-
-
   return (
     <div className="homepage-container">
       <div className="homepage-left-container">
         <NavBar />
       </div>
       <div className="homepage-mid-container">
-        <BuzzForm />
-        <Buzzes />
+        <div className="homepage-mid-top">
+          <div className="home-text">Home</div>
+          <BuzzForm />
+        </div>
+        <div className="homepage-mid-bottom">
+          <Buzzes />
+        </div>
       </div>
-      <div className="homepage-right-container">
-      </div>
+      <div className="homepage-right-container"></div>
     </div>
   );
 };
