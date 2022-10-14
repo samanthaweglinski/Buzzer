65a5f8c514c043aca0b5fa39f61f112a9fa819f6
Samantha Weglinski <samantha.weglinski@gmail.com>
2022-10-14 13:12:55 -0700

    Clicking on user username or profile pic will take you to their page

--- react-app/src/components/Buzz.js
+++ react-app/src/components/Buzz.js
@@ -7,7 +7,6 @@ import solidHeart from "../components/images/solid_heart.svg";
 import hollowHeart from "../components/images/hollow_heart.svg";
 import { Link, NavLink } from "react-router-dom";
 
-
 const Buzz = ({ buzz, users }) => {
   const dispatch = useDispatch();
   const user = useSelector((state) => state?.session.user);
@@ -20,7 +19,6 @@ const Buzz = ({ buzz, users }) => {
   const [likedBuzz, setLikedBuzz] = useState(
     likesArray.find((like) => like.user_id === user.id)
   );
-
 
   const editBuzz = () => {
     setShowDropdown(!showDropdown);
@@ -53,93 +51,100 @@ const Buzz = ({ buzz, users }) => {
   };
 
   // console.log({users})
-  // console.log(users[buzz?.user_id - 1]?.username)
+  // console.log(buzz?.user_id)
 
   return (
     <>
       {user && user?.id == buzz?.user_id ? (
-            <>
-              <div className="buzz-content">
-                <div className="user-container">
-                  <Link
-                    to={`/users/${buzz?.user_id}`}
-                    className="user-profile-link"
-                  >
-                    <img
-                      src={users[buzz?.user_id - 1]?.profile_pic}
-                      alt=""
-                      className="buzz-pfp"
-                    />
-                    {`@${users[buzz?.user_id - 1]?.username}`}
-                  </Link>
-                </div>
-                <div>{buzz?.content}</div>
-                <img src={buzz?.image_url} className="single-buzz-img" alt="" />
-              </div>
-              <div className="buzz-options">
-                <div
-                  className="Buzzes-name"
-                  onClick={() => {
-                    editBuzz();
-                    setEditActive(!editActive);
-                  }}
-                >
-                  <button className="buzz-options-button">
-                    <i className="fa-solid fa-ellipsis fa-xl"></i>
-                  </button>
-                </div>
-                <div className="options-buttons">
-                  {showDropdown && <EditBuzzModal buzz={buzz} id={buzz.id} />}
-                  {showDropdown && <DeleteBuzzModal buzz={buzz} />}
-                </div>
-              </div>
-              <div onClick={handleLike} className={`heart-info-container`}>
-                <div className="heart-icon-container">
-                  <img
-                    className={`tweet icon heart ${
-                      likedBuzz ? "liked" : "not-liked"
-                    }`}
-                    src={likedBuzz ? solidHeart : hollowHeart}
-                    alt="heart-icon"
-                  />
-                </div>
-                <div className="comment-counter">
-                  <span>{likeCounter}</span>
-                </div>
-              </div>
-            </>
-          ) : (
-            <>
-              <div className="buzz-content">
-                <div className="user-container">
-                  <img
-                    src={users[buzz?.user_id - 1]?.profile_pic}
-                    alt=""
-                    className="buzz-pfp"
-                  />
-                  {`@${users[buzz?.user_id - 1]?.username}`}
-                </div>
-                <div>{buzz?.content}</div>
-                <img src={buzz?.image_url} className="single-buzz-img" alt="" />
-              </div>
-              <div onClick={handleLike} className={`heart-info-container`}>
-                <div className="heart-icon-container">
-                  <img
-                    className={`tweet icon heart ${
-                      likedBuzz ? "liked" : "not-liked"
-                    }`}
-                    src={likedBuzz ? solidHeart : hollowHeart}
-                    alt="heart-icon"
-                  />
-                </div>
-                <div className="comment-counter">
-                  <span>{likeCounter}</span>
-                </div>
-              </div>
-            </>
-          )}
+        <>
+          <div className="buzz-content">
+            <div className="user-container">
+              <Link
+                to={`/users/${buzz?.user_id}`}
+                className="user-profile-link"
+              >
+                <img
+                  src={users[buzz?.user_id - 1]?.profile_pic}
+                  alt=""
+                  className="buzz-pfp"
+                />
+                {`@${users[buzz?.user_id - 1]?.username}`}
+              </Link>
+            </div>
+            <div className="content-container">
+              <div>{buzz?.content}</div>
+              <img src={buzz?.image_url} className="single-buzz-img" alt="" />
+            </div>
+          </div>
+          <div className="buzz-options">
+            <div
+              className="Buzzes-name"
+              onClick={() => {
+                editBuzz();
+                setEditActive(!editActive);
+              }}
+            >
+              <button className="buzz-options-button">
+                <i className="fa-solid fa-ellipsis fa-xl"></i>
+              </button>
+            </div>
+            <div className="options-buttons">
+              {showDropdown && <EditBuzzModal buzz={buzz} id={buzz.id} />}
+              {showDropdown && <DeleteBuzzModal buzz={buzz} />}
+            </div>
+          </div>
+          <div onClick={handleLike} className={`heart-info-container`}>
+            <div className="heart-icon-container">
+              <img
+                className={`tweet icon heart ${
+                  likedBuzz ? "liked" : "not-liked"
+                }`}
+                src={likedBuzz ? solidHeart : hollowHeart}
+                alt="heart-icon"
+              />
+            </div>
+            <div className="comment-counter">
+              <span>{likeCounter}</span>
+            </div>
+          </div>
+        </>
+      ) : (
+        <>
+          <div className="buzz-content">
+            <div className="user-container">
+              <Link
+                to={`/users/${buzz?.user_id}`}
+                className="user-profile-link"
+              >
+                <img
+                  src={users[buzz?.user_id - 1]?.profile_pic}
+                  alt=""
+                  className="buzz-pfp"
+                />
+                {`@${users[buzz?.user_id - 1]?.username}`}
+              </Link>
+            </div>
+            <div>{buzz?.content}</div>
+            <img src={buzz?.image_url} className="single-buzz-img" alt="" />
+          </div>
+          <div onClick={handleLike} className={`heart-info-container`}>
+            <div className="heart-icon-container">
+              <img
+                className={`tweet icon heart ${
+                  likedBuzz ? "liked" : "not-liked"
+                }`}
+                src={likedBuzz ? solidHeart : hollowHeart}
+                alt="heart-icon"
+              />
+            </div>
+            <div className="comment-counter">
+              <span>{likeCounter}</span>
+            </div>
+          </div>
+        </>
+      )}
     </>
-  )
+  );
 };
 
 export default Buzz;
